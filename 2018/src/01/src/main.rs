923d43ef98a46af7660392d1e9547af6db410388
Marshall Bowers <elliott.codes@gmail.com>
2018-12-01 09:49:18 -0500

    Solve Day 1: Part Two

--- 2018/src/01/src/main.rs
+++ 2018/src/01/src/main.rs
@@ -1,9 +1,27 @@
+use std::collections::{HashSet, VecDeque};
 use std::fs::File;
 use std::io::Read;
 
+#[derive(Debug, Clone)]
 enum FrequencyChange {
     Increase(i32),
     Decrease(i32),
+}
+
+fn read_frequency_changes_from_input(input: &String) -> Vec<FrequencyChange> {
+    input
+        .lines()
+        .map(|line| {
+            let mut characters = line.chars();
+            let sign = characters.next().unwrap();
+            let number = characters.collect::<String>().parse::<i32>().unwrap();
+            match sign {
+                '+' => FrequencyChange::Increase(number),
+                '-' => FrequencyChange::Decrease(number),
+                c => panic!("Unexpected character '{}'", c),
+            }
+        })
+        .collect()
 }
 
 fn calculate_frequency(changes: Vec<FrequencyChange>) -> i32 {
@@ -13,22 +31,38 @@ fn calculate_frequency(changes: Vec<FrequencyChange>) -> i32 {
     })
 }
 
-fn part_one(input: String) -> i32 {
-    calculate_frequency(
-        input
-            .lines()
-            .map(|line| {
-                let mut characters = line.chars();
-                let sign = characters.next().unwrap();
-                let number = characters.collect::<String>().parse::<i32>().unwrap();
-                match sign {
-                    '+' => FrequencyChange::Increase(number),
-                    '-' => FrequencyChange::Decrease(number),
-                    c => panic!("Unexpected character '{}'", c),
-                }
-            })
-            .collect(),
-    )
+fn find_first_repeated_frequency(changes: Vec<FrequencyChange>) -> i32 {
+    let mut already_seen = HashSet::<i32>::new();
+    let mut pending_changes = VecDeque::from(changes.clone());
+
+    let mut frequency = 0;
+    already_seen.insert(frequency);
+
+    loop {
+        if pending_changes.is_empty() {
+            pending_changes.clone_from(&VecDeque::from(changes.clone()));
+        }
+
+        let next_frequency = match pending_changes.pop_front().unwrap() {
+            FrequencyChange::Increase(n) => frequency + n,
+            FrequencyChange::Decrease(n) => frequency - n,
+        };
+
+        if already_seen.contains(&next_frequency) {
+            break next_frequency;
+        }
+
+        already_seen.insert(next_frequency);
+        frequency = next_frequency;
+    }
+}
+
+fn part_one(input: &String) -> i32 {
+    calculate_frequency(read_frequency_changes_from_input(input))
+}
+
+fn part_two(input: &String) -> i32 {
+    find_first_repeated_frequency(read_frequency_changes_from_input(input))
 }
 
 fn main() -> std::io::Result<()> {
@@ -36,7 +70,8 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    println!("Part One: {}", part_one(contents));
+    println!("Part One: {}", part_one(&contents));
+    println!("Part Two: {}", part_two(&contents));
 
     Ok(())
 }
@@ -54,5 +89,12 @@ mod tests {
         ];
 
         assert_eq!(calculate_frequency(changes), 3)
+    }
+
+    #[test]
+    fn test_find_first_repeated_frequency() {
+        let changes = vec![FrequencyChange::Increase(1), FrequencyChange::Decrease(1)];
+
+        assert_eq!(find_first_repeated_frequency(changes), 0)
     }
 }
