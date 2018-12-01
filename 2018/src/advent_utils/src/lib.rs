eef67b275b1dede4d070b6190e1d0149d8b503bc
Marshall Bowers <elliott.codes@gmail.com>
2018-12-01 12:52:12 -0500

    Remove local advent_utils

--- 2018/src/advent_utils/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use std::fs::File;
-use std::io::Read;
-
-pub fn read_input(path: &str) -> std::io::Result<String> {
-    let mut file = File::open(path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    Ok(contents)
-}
