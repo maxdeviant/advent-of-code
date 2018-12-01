646443458b4ae8694a4aad862477a14460bbd12d
Marshall Bowers <elliott.codes@gmail.com>
2018-12-01 11:30:26 -0500

    Day 2 prep

---
+++ 2018/src/02/src/main.rs
@@ -0,0 +1,13 @@
+extern crate advent_utils;
+
+fn part_one(input: &str) {}
+
+fn part_two(input: &str) {}
+
+fn main() {
+    fn main() -> std::io::Result<()> {
+        let input = advent_utils::read_input("input.txt")?;
+
+        Ok(())
+    }
+}
