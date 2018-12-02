68588f1c3550b384c6bd8bb4459bc9faec5837d4
Marshall Bowers <elliott.codes@gmail.com>
2018-12-02 01:57:00 -0500

    Solve Day 2: Part Two

--- 2018/src/02/src/main.rs
+++ 2018/src/02/src/main.rs
@@ -45,12 +45,41 @@ fn part_one(input: &Input) -> usize {
     twos * threes
 }
 
-fn part_two(input: &Input) {}
+fn diff_ids(a: &str, b: &str) -> Vec<usize> {
+    let mut differences = Vec::new();
+
+    for (index, character) in a.char_indices() {
+        for (index_b, character_b) in b.char_indices() {
+            if index == index_b && character != character_b {
+                differences.push(index);
+            }
+        }
+    }
+
+    differences
+}
+
+fn part_two(input: &Input) -> Option<String> {
+    for line in input.value.lines() {
+        for line_b in input.value.lines() {
+            let diff = diff_ids(line, line_b);
+            if diff.len() == 1 {
+                let differing_index = diff.first().unwrap();
+                let mut characters: Vec<char> = line.chars().collect();
+                characters.remove(*differing_index);
+                return Some(characters.iter().collect());
+            }
+        }
+    }
+
+    None
+}
 
 fn main() -> std::io::Result<()> {
     let input = Input::from_file("input.txt")?;
 
     println!("Part One: {}", part_one(&input));
+    println!("Part Two: {}", part_two(&input).unwrap());
 
     Ok(())
 }
@@ -70,6 +99,14 @@ mod tests {
     fn test_part_two_solution() -> std::io::Result<()> {
         let input = Input::from_file("input.txt")?;
 
-        Ok(())
+        Ok(assert_eq!(
+            part_two(&input),
+            Some(String::from("wmlnjevbfodamyiqpucrhsukg"))
+        ))
+    }
+
+    #[test]
+    fn test_diff_ids() {
+        assert_eq!(diff_ids("fghij", "fguij"), vec![2])
     }
 }
