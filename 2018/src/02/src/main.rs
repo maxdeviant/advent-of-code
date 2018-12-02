341f99ef147aab53ae886236cbfe855969783511
Marshall Bowers <elliott.codes@gmail.com>
2018-12-02 01:02:50 -0500

    Solve Day 2: Part One

--- 2018/src/02/src/main.rs
+++ 2018/src/02/src/main.rs
@@ -1,17 +1,58 @@
 extern crate adventurous;
 
+use std::collections::HashMap;
+
 use adventurous::Input;
 
-fn part_one(input: &Input) {}
+fn count_letters(id: &str) -> HashMap<char, i32> {
+    let mut letter_counts = HashMap::new();
+
+    for letter in id.chars() {
+        if !letter_counts.contains_key(&letter) {
+            letter_counts.insert(letter, 0);
+        }
+
+        *letter_counts.get_mut(&letter).unwrap() += 1;
+    }
+
+    letter_counts
+}
+
+fn contains_any_letter_two_times(id: &str) -> bool {
+    count_letters(id)
+        .into_iter()
+        .any(|(_letter, occurences)| occurences == 2)
+}
+
+fn contains_any_letter_three_times(id: &str) -> bool {
+    count_letters(id)
+        .into_iter()
+        .any(|(_letter, occurrences)| occurrences == 3)
+}
+
+fn part_one(input: &Input) -> usize {
+    let twos = input
+        .value
+        .lines()
+        .filter(|line| contains_any_letter_two_times(line))
+        .count();
+    let threes = input
+        .value
+        .lines()
+        .filter(|line| contains_any_letter_three_times(line))
+        .count();
+
+    twos * threes
+}
 
 fn part_two(input: &Input) {}
 
-fn main() {
-    fn main() -> std::io::Result<()> {
-        let input = Input::from_file("input.txt")?;
+fn main() -> std::io::Result<()> {
+    let input = Input::from_file("input.txt")?;
 
-        Ok(())
-    }
+    println!("Part One: {}", part_one(&input));
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -22,7 +63,7 @@ mod tests {
     fn test_part_one_solution() -> std::io::Result<()> {
         let input = Input::from_file("input.txt")?;
 
-        Ok(())
+        Ok(assert_eq!(part_one(&input), 7350))
     }
 
     #[test]
