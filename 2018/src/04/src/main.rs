e0a5a9d54a11d4871f1613565815750098f967ac
Marshall Bowers <elliott.codes@gmail.com>
2018-12-04 14:38:48 -0500

    Add Day 4

---
+++ 2018/src/04/src/main.rs
@@ -0,0 +1,15 @@
+extern crate adventurous;
+
+use adventurous::Input;
+
+fn part_one(input: &Input) -> i32 {
+    0
+}
+
+fn main() -> std::io::Result<()> {
+    let input = Input::from_file("input.txt")?;
+
+    println!("Part One: {}", part_one(&input));
+
+    Ok(())
+}
