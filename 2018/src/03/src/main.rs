a634f755f12efef46f8def5acee6023489f05e74
Marshall Bowers <elliott.codes@gmail.com>
2018-12-03 21:19:04 -0500

    Add claim parsing

--- 2018/src/03/src/main.rs
+++ 2018/src/03/src/main.rs
@@ -2,8 +2,74 @@ extern crate adventurous;
 
 use adventurous::Input;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct Claim {
+    pub id: String,
+    pub x: i32,
+    pub y: i32,
+    pub width: i32,
+    pub height: i32,
+}
+
+impl Claim {
+    pub fn from_str(value: &str) -> Self {
+        let mut parts = value.split_whitespace();
+        let id_part = parts.next();
+        let _ = parts.next();
+        let coordinates_part = parts.next();
+        let dimensions_part = parts.next();
+
+        let (x, y) = {
+            let coordinates = coordinates_part
+                .map(|x| {
+                    x[..x.len() - 1]
+                        .split(',')
+                        .map(|x| x.parse::<i32>().unwrap())
+                        .collect()
+                })
+                .unwrap_or(vec![]);
+            match coordinates.as_slice() {
+                &[x, y] => (x, y),
+                _ => unreachable!(),
+            }
+        };
+
+        let (width, height) = {
+            let dimensions = dimensions_part
+                .map(|x| x.split('x').map(|x| x.parse::<i32>().unwrap()).collect())
+                .unwrap_or(vec![]);
+            match dimensions.as_slice() {
+                &[width, height] => (width, height),
+                _ => unreachable!(),
+            }
+        };
+
+        Self {
+            id: id_part.unwrap_or("").to_string(),
+            x,
+            y,
+            width,
+            height,
+        }
+    }
+}
+
+fn part_one(input: &Input) -> i32 {
+    let claims = input.value.lines().map(Claim::from_str);
+
+    for claim in claims {
+        println!("{:?}", claim);
+    }
+
+    0
+}
+
+fn main() -> std::io::Result<()> {
+    let input = Input::from_file("input.txt")?;
+
+    println!("Part One: {}", part_one(&input));
+
+    Ok(())
 }
 
 #[cfg(test)]
