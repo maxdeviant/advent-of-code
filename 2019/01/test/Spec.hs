4b3a3062249de6aaa05a8d0c23dbf937d9cfe051
Marshall Bowers <elliott.codes@gmail.com>
2019-12-01 14:36:32 -0500

    Add tests

--- 2019/01/test/Spec.hs
+++ 2019/01/test/Spec.hs
@@ -1,2 +1,13 @@
+import Lib
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $ do
+    it "works for sample input: 12" $ do partOne "12" `shouldBe` 2
+    it "returns the correct answer for Part One" $ do
+      input <- readFile "input.txt"
+      partOne input `shouldBe` 3423511
+    it "returns the correct answer for Part Two" $ do
+      input <- readFile "input.txt"
+      partTwo input `shouldBe` 5132379
