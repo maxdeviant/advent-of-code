6261ba4236dd0278d7f47fbde8c7f45e73f112b0
Marshall Bowers <elliott.codes@gmail.com>
2019-12-01 14:16:20 -0500

    Solve Day 1: Part Two

--- 2019/01/app/Main.hs
+++ 2019/01/app/Main.hs
@@ -5,4 +5,7 @@ import Lib
 main :: IO ()
 main = do
   input <- readFile "input.txt"
+  print "Part One"
   print $ partOne input
+  print "Part Two"
+  print $ partTwo input
--- 2019/01/src/Lib.hs
+++ 2019/01/src/Lib.hs
@@ -1,9 +1,24 @@
 module Lib
   ( partOne
+  , partTwo
   ) where
 
 import Control.Arrow ((>>>))
 
+requiredFuel :: Double -> Int
 requiredFuel = (/ 3) >>> floor >>> (subtract 2)
 
+partOne :: String -> Int
 partOne = lines >>> map read >>> map requiredFuel >>> foldr (+) 0
+
+actualRequiredFuel :: Double -> Int
+actualRequiredFuel = actualRequiredFuel' 0
+  where
+    actualRequiredFuel' acc mass =
+      let fuel = requiredFuel mass
+       in if fuel <= 0
+            then acc
+            else actualRequiredFuel' (acc + fuel) $ fromIntegral fuel
+
+partTwo :: String -> Int
+partTwo = lines >>> map read >>> map actualRequiredFuel >>> foldr (+) 0
