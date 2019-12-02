05d4e9a7f366cc16187d559a9835fd9860c454fa
Marshall Bowers <elliott.codes@gmail.com>
2019-12-02 07:49:32 -0500

    Solve Day 2: Part One

--- 2019/02/src/Lib.hs
+++ 2019/02/src/Lib.hs
@@ -1,6 +1,33 @@
 module Lib
-    ( someFunc
-    ) where
+  ( partOne
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List.Index (setAt)
+import Data.List.Split (splitOn)
+
+evalIntcode :: [Int] -> [Int]
+evalIntcode program = evalIntcode' 0 program
+  where
+    evalIntcode' readPosition instructions =
+      case drop readPosition instructions of
+        [] -> instructions
+        99:_ -> instructions
+        opcode:positionA:positionB:outputPosition:_ ->
+          let operation =
+                case opcode of
+                  1 -> (+)
+                  2 -> (*)
+              inputA = instructions !! positionA
+              inputB = instructions !! positionB
+           in evalIntcode'
+                (readPosition + 4)
+                (setAt outputPosition (operation inputA inputB) instructions)
+
+restoreProgramAlarmState :: [Int] -> [Int]
+restoreProgramAlarmState = setAt 2 2 . setAt 1 12
+
+parseIntcode :: String -> [Int]
+parseIntcode = map read . splitOn ","
+
+partOne :: String -> Int
+partOne = head . evalIntcode . restoreProgramAlarmState . parseIntcode
