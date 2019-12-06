c9668d09d80f3ae51776550fd0a1170ce1f7d1cb
Marshall Bowers <elliott.codes@gmail.com>
2019-12-06 14:04:54 +0900

    Finish Day 4: Part One

--- 2019/04/src/Lib.hs
+++ 2019/04/src/Lib.hs
@@ -1,6 +1,41 @@
 module Lib
-    ( someFunc
-    ) where
+  ( partOne
+  , digits
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List.Split (splitOn)
+
+digits :: Integral a => a -> [a]
+digits 0 = []
+digits n = digits (n `div` 10) ++ [n `mod` 10]
+
+containsDoubles :: Int -> Bool
+containsDoubles n = containsDoubles' $ digits n
+  where
+    containsDoubles' [] = False
+    containsDoubles' (_:[]) = False
+    containsDoubles' (x:y:rest) =
+      if x == y
+        then True
+        else containsDoubles' (y : rest)
+
+neverDecreases :: Int -> Bool
+neverDecreases n = neverDecreases' $ digits n
+  where
+    neverDecreases' [] = True
+    neverDecreases' (_:[]) = True
+    neverDecreases' (x:y:rest) =
+      if y < x
+        then False
+        else neverDecreases' (y : rest)
+
+possiblePasswords :: Int -> Int -> [Int]
+possiblePasswords lower upper =
+  filter containsDoubles . filter neverDecreases $ allPasswords
+  where
+    allPasswords = [lower .. upper]
+
+partOne :: String -> Int
+partOne input = length $ possiblePasswords (read lower) (read upper)
+  where
+    (lower:upper:[]) = splitOn "-" input
