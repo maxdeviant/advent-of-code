e5ea4eb8d9bfdae523d71d92a649a2fc388359e3
Marshall Bowers <elliott.codes@gmail.com>
2020-12-01 20:27:36 -0500

    Complete 2020 Day 1: Part 1

--- 2020/purescript/01/spago.dhall
+++ 2020/purescript/01/spago.dhall
@@ -1,5 +1,6 @@
 { name = "day-1"
-, dependencies = [ "console", "effect", "node-fs", "psci-support" ]
+, dependencies =
+  [ "console", "effect", "integers", "node-fs", "psci-support", "stringutils" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- 2020/purescript/01/src/Main.purs
+++ 2020/purescript/01/src/Main.purs
@@ -1,14 +1,35 @@
 module Main where
 
 import Prelude
-import Data.Either (Either(..))
+import Data.Array (filter, head)
+import Data.Either (Either(..), note)
+import Data.Int as Int
+import Data.String.Utils (lines)
+import Data.Traversable (traverse)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
+parseExpenseReport :: String -> Either String (Array Int)
+parseExpenseReport =
+  lines
+    >>> traverse
+        ( \line ->
+            line # Int.fromString # note ("Failed to parse " <> line)
+        )
+
 partOne :: String -> Either String Int
-partOne input = Left "Part One not implemented."
+partOne input = do
+  let
+    sumToFind = 2020
+  expenseReport <- parseExpenseReport input
+  [ Tuple ] <*> expenseReport <*> expenseReport
+    # filter (\(Tuple x y) -> x + y == sumToFind)
+    # map (\(Tuple x y) -> x * y)
+    # head
+    # (note $ "No entries found that sum to " <> show sumToFind)
 
 partTwo :: String -> Either String Int
 partTwo input = Left "Part Two not implemented."
