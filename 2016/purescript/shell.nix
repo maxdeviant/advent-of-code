5c46b240e512899906663b32743406e3a47b6bc8
Marshall Bowers <elliott.codes@gmail.com>
2020-12-03 21:07:51 -0500

    Setup 2016

---
+++ 2016/purescript/shell.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+
+let
+  easy-ps = import (fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "7b1c1635e16c7f12065db2f8ec049030fcc63655";
+    sha256 = "1nybcann9aiwbvj95p6wam8xyhxwaxmfnkxmgylxcw42np2lvbzr";
+  }) {
+    inherit pkgs;
+  };
+in
+stdenv.mkDerivation {
+  name = "advent-of-code-2016";
+
+  buildInputs = [
+    easy-ps.purs
+    easy-ps.spago
+    easy-ps.purty
+    nodejs
+    yarn
+  ];
+}
