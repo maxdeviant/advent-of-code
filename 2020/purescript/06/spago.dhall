5e358092d66e854d47397b8d5d6548c3da49bb8f
Marshall Bowers <elliott.codes@gmail.com>
2020-12-06 10:46:49 -0500

    Solve 2020 Day 6: Part 1

--- 2020/purescript/06/spago.dhall
+++ 2020/purescript/06/spago.dhall
@@ -1,6 +1,13 @@
 { name = "day-6"
 , dependencies =
-  [ "console", "effect", "integers", "node-fs", "psci-support", "stringutils" ]
+  [ "console"
+  , "effect"
+  , "integers"
+  , "node-fs"
+  , "ordered-collections"
+  , "psci-support"
+  , "stringutils"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- 2020/purescript/06/src/Main.purs
+++ 2020/purescript/06/src/Main.purs
@@ -1,14 +1,56 @@
 module Main where
 
 import Prelude
+import Data.Array (concatMap, uncons, (:))
+import Data.Array as Array
 import Data.Either (Either(..))
+import Data.Foldable (foldl, sum)
+import Data.Maybe (Maybe(..))
+import Data.Set (Set)
+import Data.Set as Set
+import Data.String.CodeUnits (toCharArray)
+import Data.String.Utils (lines)
 import Effect (Effect)
 import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
+type Answers
+  = Set Char
+
+type Person
+  = { answers :: Answers }
+
+parsePerson :: String -> Person
+parsePerson = toCharArray >>> Set.fromFoldable >>> { answers: _ }
+
+type Group
+  = Array Person
+
+groupAnswers :: Group -> Answers
+groupAnswers =
+  foldl (flip Set.insert) Set.empty
+    <<< concatMap (Array.fromFoldable <<< _.answers)
+
+parseGroups :: String -> Array Group
+parseGroups = parseGroups' [] [] <<< lines
+  where
+  parseGroups' groups currentGroup lines = case uncons lines of
+    Just { head: "", tail } -> parseGroups' (currentGroup : groups) [] tail
+    Just { head, tail } ->
+      let
+        person = parsePerson head
+      in
+        parseGroups' groups (person : currentGroup) tail
+    Nothing -> Array.reverse (currentGroup : groups)
+
 partOne :: String -> Either String Int
-partOne input = Left "Part One not implemented."
+partOne =
+  parseGroups
+    >>> map groupAnswers
+    >>> map Set.size
+    >>> sum
+    >>> pure
 
 partTwo :: String -> Either String Int
 partTwo input = Left "Part Two not implemented."
