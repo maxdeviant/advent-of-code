f8b3c04895ff56cb33b8e2a40663449e59fb9dff
Marshall Bowers <elliott.codes@gmail.com>
2021-12-01 20:11:26 -0500

    2021(purescript): Solve Day 1: Part 1

--- 2021/purescript/spago.dhall
+++ 2021/purescript/spago.dhall
@@ -1,14 +1,20 @@
 { name = "advent-of-code-2021"
 , dependencies =
   [ "aff"
+  , "arrays"
   , "console"
   , "effect"
   , "either"
+  , "foldable-traversable"
+  , "integers"
+  , "maybe"
+  , "newtype"
   , "node-buffer"
   , "node-fs"
   , "prelude"
   , "psci-support"
   , "spec"
+  , "stringutils"
   ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
--- 2021/purescript/src/Day01/Main.purs
+++ 2021/purescript/src/Day01/Main.purs
@@ -2,14 +2,44 @@ module Day01.Main where
 
 import Prelude
 
-import Data.Either (Either(..))
+import Data.Array (uncons)
+import Data.Either (Either(..), note)
+import Data.Int as Int
+import Data.Maybe (Maybe(..))
+import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.String.Utils (lines)
+import Data.Traversable (traverse)
 import Effect (Effect)
 import Effect.Console (log, logShow)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
+newtype SonarSweepReport = SonarSweepReport (Array Int)
+
+derive instance Newtype SonarSweepReport _
+
+parseReport :: String -> Either String SonarSweepReport
+parseReport = lines >>> traverse parseLine >>> map wrap
+  where
+  parseLine line = line # Int.fromString # note ("Invalid depth: " <> line)
+
+countDepthIncreases :: SonarSweepReport -> Int
+countDepthIncreases = unwrap >>> go 0 Nothing
+  where
+  go acc lastDepth depths =
+    case uncons depths of
+      Just { head: depth, tail } ->
+        case lastDepth of
+          Nothing -> go acc (Just depth) tail
+          Just lastDepth' ->
+            let
+              increase = if depth > lastDepth' then 1 else 0
+            in
+              go (acc + increase) (Just depth) tail
+      Nothing -> acc
+
 partOne :: String -> Either String Int
-partOne input = Left "Part One not implemented."
+partOne = parseReport >>> map countDepthIncreases
 
 partTwo :: String -> Either String Int
 partTwo input = Left "Part Two not implemented."
