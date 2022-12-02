d02ee99fff22f721dc8237025d5199abb4e06e20
Marshall Bowers <elliott.codes@gmail.com>
2022-12-02 09:12:59 -0500

    2022(rust): Solve Day 2: Part 1

---
+++ 2022/rust/days/day_02/src/main.rs
@@ -0,0 +1,152 @@
+use std::str::FromStr;
+
+use adventurous::Input;
+
+trait Scoring {
+    fn get_score(&self) -> i32;
+}
+
+#[derive(Debug)]
+enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Scoring for Shape {
+    fn get_score(&self) -> i32 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+}
+
+impl FromStr for Shape {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Self::Rock),
+            "B" | "Y" => Ok(Self::Paper),
+            "C" | "Z" => Ok(Self::Scissors),
+            _ => Err(format!("'{}' is not a valid shape.", s)),
+        }
+    }
+}
+
+/// The outcome of a [`Round`] from the persective of the active player.
+#[derive(Debug)]
+enum RoundOutcome {
+    Won,
+    Lost,
+    Draw,
+}
+
+#[derive(Debug)]
+struct Round {
+    opponent: Shape,
+    counter: Shape,
+}
+
+impl Round {
+    pub fn outcome(&self) -> RoundOutcome {
+        match (&self.counter, &self.opponent) {
+            (Shape::Rock, Shape::Scissors)
+            | (Shape::Scissors, Shape::Paper)
+            | (Shape::Paper, Shape::Rock) => RoundOutcome::Won,
+            (Shape::Rock, Shape::Paper)
+            | (Shape::Paper, Shape::Scissors)
+            | (Shape::Scissors, Shape::Rock) => RoundOutcome::Lost,
+            (Shape::Rock, Shape::Rock)
+            | (Shape::Paper, Shape::Paper)
+            | (Shape::Scissors, Shape::Scissors) => RoundOutcome::Draw,
+        }
+    }
+}
+
+impl Scoring for Round {
+    fn get_score(&self) -> i32 {
+        let shape_score = self.counter.get_score();
+        let round_score = match self.outcome() {
+            RoundOutcome::Lost => 0,
+            RoundOutcome::Draw => 3,
+            RoundOutcome::Won => 6,
+        };
+
+        shape_score + round_score
+    }
+}
+
+impl FromStr for Round {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut moves = s.split(" ").take(2);
+
+        let opponent = moves
+            .next()
+            .ok_or_else(|| format!("no move for opponent"))
+            .and_then(|shape| shape.parse())?;
+        let counter = moves
+            .next()
+            .ok_or_else(|| format!("no move for counter"))
+            .and_then(|shape| shape.parse())?;
+
+        Ok(Self { opponent, counter })
+    }
+}
+
+fn part_one(input: &Input) -> i32 {
+    let rounds = input
+        .value
+        .lines()
+        .map(Round::from_str)
+        .collect::<Result<Vec<_>, _>>()
+        .expect("failed to parse input");
+
+    rounds.into_iter().map(|round| round.get_score()).sum()
+}
+
+fn part_two(input: &Input) -> i32 {
+    0
+}
+
+fn main() -> std::io::Result<()> {
+    let input = Input::from_file("input.txt")?;
+
+    println!("Part One: {}", part_one(&input));
+    println!("Part Two: {}", part_two(&input));
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_solution() -> std::io::Result<()> {
+        let input = Input::from_file("input.txt")?;
+
+        Ok(assert_eq!(part_one(&input), 14069))
+    }
+
+    #[test]
+    #[ignore = "Part 2 not yet done"]
+    fn test_part_two_solution() -> std::io::Result<()> {
+        let input = Input::from_file("input.txt")?;
+
+        Ok(assert_eq!(part_two(&input), 0))
+    }
+
+    #[test]
+    fn test_part_one_sample_input() {
+        let input = Input {
+            value: "A Y\nB X\nC Z".to_string(),
+        };
+
+        assert_eq!(part_one(&input), 15);
+    }
+}
