093e5717f2696b4fc621fd8a35a2b015184a9a90
Marshall Bowers <elliott.codes@gmail.com>
2023-12-03 10:44:36 -0500

    2023(rust): Solve Day 3: Part 2

--- 2023/rust/days/day_03/src/main.rs
+++ 2023/rust/days/day_03/src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashSet;
+use std::iter::Sum;
 
 use adventurous::Input;
 use anyhow::Result;
+use indexmap::IndexMap;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
 enum Glyph {
@@ -55,13 +56,23 @@ impl Span {
     }
 }
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+pub struct PartNumber(usize);
+
+impl Sum<PartNumber> for usize {
+    fn sum<I: Iterator<Item = PartNumber>>(iter: I) -> Self {
+        iter.fold(0, |acc, part_number| acc + part_number.0)
+    }
+}
+
 struct Schematic {
-    glyphs_with_coordinates: Vec<(Point, Glyph)>,
+    glyphs_by_coordinates: IndexMap<Point, Glyph>,
+    symbols_by_coordinates: IndexMap<Point, char>,
 }
 
 impl Schematic {
     pub fn parse(input: &Input) -> Self {
-        let glyphs_with_coordinates = input
+        let glyphs_by_coordinates = input
             .lines()
             .enumerate()
             .flat_map(|(y, line)| {
@@ -70,11 +81,103 @@ impl Schematic {
                     .enumerate()
                     .map(move |(x, glyph)| (Point::xy(x, y), glyph))
             })
-            .collect();
+            .collect::<IndexMap<_, _>>();
+
+        let symbols_by_coordinates = glyphs_by_coordinates
+            .iter()
+            .filter_map(|(coords, glyph)| match glyph {
+                Glyph::Symbol(symbol) => Some((*coords, *symbol)),
+                _ => None,
+            })
+            .collect::<IndexMap<_, _>>();
 
         Self {
-            glyphs_with_coordinates,
+            glyphs_by_coordinates,
+            symbols_by_coordinates,
         }
+    }
+
+    pub fn part_numbers(&self) -> impl Iterator<Item = PartNumber> {
+        self.spanned_part_numbers()
+            .into_iter()
+            .map(|(part_number, _span)| part_number)
+    }
+
+    fn spanned_part_numbers(&self) -> Vec<(PartNumber, Span)> {
+        let mut spanned_numbers = Vec::new();
+        let mut current_number = Vec::new();
+
+        for (coords, glyph) in &self.glyphs_by_coordinates {
+            if let Glyph::Digit(digit) = glyph {
+                current_number.push((coords, digit));
+                continue;
+            }
+
+            if current_number.is_empty() {
+                continue;
+            }
+
+            let (coords, digits): (Vec<Point>, Vec<&char>) =
+                current_number.clone().into_iter().unzip();
+
+            let part_number = digits
+                .into_iter()
+                .collect::<String>()
+                .parse::<usize>()
+                .unwrap();
+
+            let lo = coords.iter().min().cloned().unwrap();
+            let hi = coords.iter().max().cloned().unwrap();
+
+            spanned_numbers.push((part_number, Span::new(lo, hi)));
+
+            current_number.clear();
+        }
+
+        spanned_numbers
+            .into_iter()
+            .filter_map(move |(number, span)| {
+                let is_adjacent_to_symbol = self
+                    .symbols_by_coordinates
+                    .keys()
+                    .find(|symbol_coords| span.adjacent_to(**symbol_coords))
+                    .is_some();
+
+                Some((PartNumber(number), span)).filter(|_| is_adjacent_to_symbol)
+            })
+            .collect()
+    }
+
+    pub fn gear_ratios(&self) -> Vec<usize> {
+        self.symbols_by_coordinates
+            .iter()
+            .filter_map(|(coords, char)| {
+                if *char != '*' {
+                    return None;
+                }
+
+                let adjacent_part_numbers = self
+                    .spanned_part_numbers()
+                    .into_iter()
+                    .filter_map(|(part_number, span)| {
+                        if span.adjacent_to(*coords) {
+                            return Some(part_number);
+                        }
+
+                        None
+                    })
+                    .collect::<Vec<_>>();
+
+                if adjacent_part_numbers.len() != 2 {
+                    return None;
+                }
+
+                let part_number_one = adjacent_part_numbers[0];
+                let part_number_two = adjacent_part_numbers[1];
+
+                Some(part_number_one.0 * part_number_two.0)
+            })
+            .collect()
     }
 }
 
@@ -82,59 +185,15 @@ impl Schematic {
 fn part_one(input: &Input) -> Result<usize> {
     let schematic = Schematic::parse(&input);
 
-    let mut spanned_numbers = Vec::new();
-    let mut current_number = Vec::new();
-
-    for (coords, glyph) in &schematic.glyphs_with_coordinates {
-        if let Glyph::Digit(digit) = glyph {
-            current_number.push((coords, digit));
-            continue;
-        }
-
-        if current_number.is_empty() {
-            continue;
-        }
-
-        let (coords, digits): (Vec<Point>, Vec<&char>) = current_number.clone().into_iter().unzip();
-
-        let part_number = digits
-            .into_iter()
-            .collect::<String>()
-            .parse::<usize>()
-            .unwrap();
-
-        let lo = coords.iter().min().cloned().unwrap();
-        let hi = coords.iter().max().cloned().unwrap();
-
-        spanned_numbers.push((part_number, Span::new(lo, hi)));
-
-        current_number.clear();
-    }
-
-    let symbol_coordinates = schematic
-        .glyphs_with_coordinates
-        .iter()
-        .filter_map(|(coords, glyph)| match glyph {
-            Glyph::Symbol(_) => Some(*coords),
-            _ => None,
-        })
-        .collect::<HashSet<_>>();
-
-    let part_numbers = spanned_numbers.into_iter().filter_map(|(number, span)| {
-        let is_adjacent_to_symbol = symbol_coordinates
-            .iter()
-            .find(|symbol_coords| span.adjacent_to(**symbol_coords))
-            .is_some();
-
-        Some(number).filter(|_| is_adjacent_to_symbol)
-    });
-
-    Ok(part_numbers.sum())
+    Ok(schematic.part_numbers().sum())
 }
 
-#[adventurous::part_two]
+#[adventurous::part_two(answer = "75312571")]
 fn part_two(input: &Input) -> Result<usize> {
-    todo!()
+    let schematic = Schematic::parse(&input);
+    let gear_ratios = schematic.gear_ratios();
+
+    Ok(gear_ratios.into_iter().sum())
 }
 
 fn main() -> Result<()> {
@@ -166,6 +225,26 @@ mod tests {
         "};
 
         assert_eq!(part_one(&Input::new(input.to_string()))?, 4361);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two_sample_input() -> Result<()> {
+        let input = indoc! {"
+            467..114..
+            ...*......
+            ..35..633.
+            ......#...
+            617*......
+            .....+.58.
+            ..592.....
+            ......755.
+            ...$.*....
+            .664.598..
+        "};
+
+        assert_eq!(part_two(&Input::new(input.to_string()))?, 467835);
 
         Ok(())
     }
