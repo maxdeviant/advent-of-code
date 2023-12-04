1b94fd8d8e61445031a5f8229517d37426a75389
Marshall Bowers <elliott.codes@gmail.com>
2023-12-03 21:03:59 -0500

    2023(rust): Setup heap profiling for days 1 and 2

--- 2023/rust/days/day_01/src/main.rs
+++ 2023/rust/days/day_01/src/main.rs
@@ -2,6 +2,13 @@ use anyhow::Result;
 
 use day_01::{part_one, part_two};
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() -> Result<()> {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     adventurous::run("days/day_01/input.txt", part_one, part_two)
 }
--- 2023/rust/days/day_02/src/main.rs
+++ 2023/rust/days/day_02/src/main.rs
@@ -2,6 +2,13 @@ use anyhow::Result;
 
 use day_02::{part_one, part_two};
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() -> Result<()> {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     adventurous::run("days/day_02/input.txt", part_one, part_two)
 }
