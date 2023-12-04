ddf812df7291f5b8619aaecc31d147ac3bb8153d
Marshall Bowers <elliott.codes@gmail.com>
2023-12-04 00:01:13 -0500

    2023(rust): Prep for Day 4

---
+++ 2023/rust/days/day_04/src/lib.rs
@@ -0,0 +1,22 @@
+use adventurous::Input;
+use anyhow::Result;
+
+#[adventurous::part_one]
+pub fn part_one(input: &Input) -> Result<usize> {
+    todo!()
+}
+
+#[adventurous::part_two]
+pub fn part_two(input: &Input) -> Result<usize> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    adventurous::test_solutions!();
+}
---
+++ 2023/rust/days/day_04/src/main.rs
@@ -0,0 +1,14 @@
+use anyhow::Result;
+
+use day_04::{part_one, part_two};
+
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
+fn main() -> Result<()> {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
+    adventurous::run("days/day_04/input.txt", part_one, part_two)
+}
