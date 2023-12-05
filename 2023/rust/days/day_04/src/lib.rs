5748a5f22fe8a8af4e50d6fdce9600704d874e6c
Marshall Bowers <elliott.codes@gmail.com>
2023-12-04 21:40:44 -0500

    2023(rust): Solve Day 4: Part 2

--- 2023/rust/days/day_04/src/lib.rs
+++ 2023/rust/days/day_04/src/lib.rs
@@ -1,9 +1,14 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use adventurous::Input;
 use anyhow::{anyhow, Result};
+use indexmap::IndexMap;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy)]
+struct CardId(usize);
 
 struct Card {
+    pub id: CardId,
     pub winning_numbers: HashSet<usize>,
     pub your_numbers: HashSet<usize>,
 }
@@ -11,12 +16,14 @@ struct Card {
 impl Card {
     pub fn parse(line: &str) -> Result<Self> {
         let mut parts = line.split(':');
-        let _card = parts.next().ok_or_else(|| anyhow!("no card identifier"))?;
+        let card = parts.next().ok_or_else(|| anyhow!("no card identifier"))?;
         let numbers = parts.next().ok_or_else(|| anyhow!("no numbers"))?;
 
         let mut parts = numbers.split('|');
         let winning_numbers = parts.next().ok_or_else(|| anyhow!("no winning numbers"))?;
         let your_numbers = parts.next().ok_or_else(|| anyhow!("no numbers you have"))?;
+
+        let id = CardId(card.replace("Card", "").trim().parse::<usize>()?);
 
         let winning_numbers = winning_numbers
             .split(' ')
@@ -33,6 +40,7 @@ impl Card {
             .collect::<Result<HashSet<_>, _>>()?;
 
         Ok(Self {
+            id,
             winning_numbers,
             your_numbers,
         })
@@ -53,6 +61,33 @@ impl Card {
 
         value
     }
+
+    pub fn matches(&self) -> usize {
+        self.winning_numbers
+            .intersection(&self.your_numbers)
+            .count()
+    }
+}
+
+fn count_copies(
+    matches_by_card: &HashMap<CardId, usize>,
+    max_card_id: CardId,
+    card_id: CardId,
+) -> usize {
+    let matches = *matches_by_card.get(&card_id).unwrap();
+
+    let mut total_copies = 0;
+
+    for i in 1..=matches {
+        let copy_id = CardId(card_id.0 + i);
+        if copy_id > max_card_id {
+            break;
+        }
+
+        total_copies += count_copies(matches_by_card, max_card_id, copy_id);
+    }
+
+    1 + total_copies
 }
 
 #[adventurous::part_one(answer = "25174")]
@@ -60,9 +95,28 @@ pub fn part_one(input: &Input) -> Result<usize> {
     Ok(input.traverse(Card::parse)?.map(|card| card.value()).sum())
 }
 
-#[adventurous::part_two]
+#[adventurous::part_two(answer = "6420979")]
 pub fn part_two(input: &Input) -> Result<usize> {
-    todo!()
+    let cards = input
+        .traverse(Card::parse)?
+        .map(|card| (card.id, card))
+        .collect::<IndexMap<_, _>>();
+
+    let max_card_id = cards
+        .keys()
+        .max()
+        .copied()
+        .ok_or_else(|| anyhow!("no max card ID"))?;
+
+    let matches_by_card = cards
+        .values()
+        .map(|card| (card.id, card.matches()))
+        .collect::<HashMap<_, _>>();
+
+    Ok(cards
+        .values()
+        .map(|card| count_copies(&matches_by_card, max_card_id, card.id))
+        .sum())
 }
 
 #[cfg(test)]
@@ -86,6 +140,22 @@ mod tests {
         "};
 
         assert_eq!(part_one(&Input::new(input.to_string()))?, 13);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two_sample_input() -> Result<()> {
+        let input = indoc! {"
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        "};
+
+        assert_eq!(part_two(&Input::new(input.to_string()))?, 30);
 
         Ok(())
     }
