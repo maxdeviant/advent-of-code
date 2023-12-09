655630bde937904cd17823c69c90695a798e5f08
Marshall Bowers <elliott.codes@gmail.com>
2023-12-09 16:48:53 -0500

    2023(rust): Init Day 7

---
+++ 2023/rust/days/day_07/src/main.rs
@@ -0,0 +1,14 @@
+use anyhow::Result;
+
+use day_07::{part_one, part_two};
+
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
+fn main() -> Result<()> {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
+    adventurous::run("days/day_07/input.txt", part_one, part_two)
+}
