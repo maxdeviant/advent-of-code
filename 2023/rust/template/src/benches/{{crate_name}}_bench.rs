8892065d72e35b5e4dfd2c1b81d9089b0d5405c2
Marshall Bowers <elliott.codes@gmail.com>
2023-12-09 16:48:37 -0500

    2023(rust): Add command to generate a crate for a new day

---
+++ 2023/rust/template/src/benches/{{crate_name}}_bench.rs
@@ -0,0 +1,18 @@
+use adventurous::Input;
+use anyhow::Result;
+
+use {{crate_name}}::{part_one, part_two};
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench(name = "part_one")]
+fn part_one_bench() -> Result<usize> {
+    part_one(divan::black_box(&Input::from(include_str!("../input.txt"))))
+}
+
+#[divan::bench(name = "part_two")]
+fn part_two_bench() -> Result<usize> {
+    part_two(divan::black_box(&Input::from(include_str!("../input.txt"))))
+}
